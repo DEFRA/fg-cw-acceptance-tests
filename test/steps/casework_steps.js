15ffd06113f3aab50cfd98005aa0dd92f4df83e9
sripappula <sripappula@gmail.com>
2025-05-30 10:30:23 +0100

    Adding new tests to check approve button (#6)
    * adding agreementName to the payload
    
    * adding new tests
    
    ---------
    
    Co-authored-by: Srikanth Pappula <srikanth.pappula@esynergy.co.uk>

--- test/steps/casework_steps.js
+++ test/steps/casework_steps.js
@@ -17,6 +17,7 @@ Given(
   async (grantName) => {
     const payloadPath = `test/payloads/${grantName}.json`
     apiResponse = await postRequest(`${grantName}/applications`, payloadPath)
+    expect(apiResponse.statusCode).toBe(201)
     return apiResponse
   }
 )
@@ -33,4 +34,18 @@ When(
 Then('the user should see the submitted application information', async () => {
   const actualApplicationText = await ApplicationPage.headerH2()
   await expect(actualApplicationText).toEqual('Application Receipt')
+})
+When('the user navigates to the Case Details section', async () => {
+  await AllcasesPage.clickLinkByText('Case Details')
+})
+Then(
+  'the user should see that the application data matches the information displayed in Casework',
+  async () => {}
+)
+When('the user Approve the application', async () => {
+  await AllcasesPage.clickButtonByText('Approve')
+})
+Then('the user should see application is successfully approved', async () => {
+  const actualApprovalText = await ApplicationPage.headerH2()
+  await expect(actualApprovalText).toEqual('Stage for contract management')
 })
