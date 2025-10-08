69e75ef087aea3622d0deac9d3e612555d126668
srikanth pappula <srikanth.pappula@esynergy.co.uk>
2025-10-08 14:56:53 +0100

    updating the URL

--- test/utils/urlResolver.js
+++ test/utils/urlResolver.js
@@ -1,7 +1,7 @@
 export default function resolveUrl(tags) {
   const environment = process.env.ENVIRONMENT || 'dev'
   if (tags.includes('@cw'))
-    return `https://fg-cw-frontend.${environment}.cdp-int.defra.cloud`
+    return `https://fg-cw-frontend.${environment}.cdp-int.defra.cloud/cases`
   if (tags.includes('@grantsui'))
     return `https://grants-ui.${environment}.cdp-int.defra.cloud/flying-pigs/start`
   return 'https://default.example.com' // fallback
