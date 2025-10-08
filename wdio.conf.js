6fb9f9ea75295ee5219f6c805f1406683e8117dd
srikanth pappula <srikanth.pappula@esynergy.co.uk>
2025-10-08 14:41:24 +0100

    Fixing tests on Pipeline

--- wdio.conf.js
+++ wdio.conf.js
@@ -87,7 +87,7 @@ export const config = {
     snippets: true,
     source: true,
     strict: false,
-    tagExpression: '@ci',
+    tagExpression: '',
     timeout: 180000,
     ignoreUndefinedDefinitions: false
   },
